17231d356cb6e732d1cb1c83eae7bf9856a2fbdc
Arafat154 <yeasinarafathasan6@gmail.com>
2021-04-21 16:31:28 +0600

    new

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
    var fixHeight = function() {
       $('.navbar-nav').css(
          'max-height',
@@ -14,5 +14,26 @@ $(document).ready(function() {
    });
    $('.navbar-toggler, .overlay').on('click', function() {
       $('.mobileMenu, .overlay').toggleClass('open');
+   });
+});*/
+
+$(".navbar-toggler").click(function() {
+   if ($(".navbar-toggler").hasClass("collapsed")) {
+      $(".collapse-bg").css({
+         display: 'none'
+      });
+   } else {
+      $(".collapse-bg").css({
+         display: 'block'
+      });
+   }
+});
+
+// click the overlay to hide the drawer
+$(".collapse-bg").click(function() {
+   $(".navbar-toggler").removeClass("collapsed");
+   $(".navbar-collapse").removeClass("show");
+   $(".collapse-bg").css({
+      display: 'none'
    });
 });
