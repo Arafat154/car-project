0c855f66380e56656110b6d743ab91e6d24ba181
Arafat154 <yeasinarafathasan6@gmail.com>
2021-04-25 07:48:10 +0600

    new

--- js/main.js
+++ js/main.js
@@ -1,7 +1,2 @@
 /*----------ripple initialization----*/
 $(function() { $(".ripple-btn, .nav-item").rippleEffect() })
-
-/*twentytwenty*/
-$(window).load(function() {
-   $("#compare").twentytwenty();
-});
