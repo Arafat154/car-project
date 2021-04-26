a50125df351bcfb46b1fe9639fc155f1e4ce9398
Arafat154 <yeasinarafathasan6@gmail.com>
2021-04-27 04:46:49 +0600

    new

--- js/main.js
+++ js/main.js
@@ -1,2 +1,5 @@
 /*----------ripple initialization----*/
-$(function() { $(".ripple-btn, .nav-item").rippleEffect() })
+$(function() { $(".ripple-btn, .nav-item").rippleEffect() });
+
+/*--------beerslider initialization------*/
+var slider = new BeerSlider( document.getElementById( "beer-slider" ) );
