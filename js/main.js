ef09b7ef81bf33338cf1fb007516c6c0a52f292a
Arafat154 <yeasinarafathasan6@gmail.com>
2021-05-02 15:59:04 +0600

    new

--- js/main.js
+++ js/main.js
@@ -6,3 +6,24 @@ var slider = new BeerSlider( document.getElementById( "beer-slider" ) );
 
 /*--------mixitup initialization----------*/
 var mixer = mixitup('.content');
+
+/*-------------owl carousel----------------*/
+$('.owl-carousel').owlCarousel({
+   loop: false,
+   margin: 10,
+   nav: false,
+   dots: true,
+   autoplay: false,
+   autoplayTimeout: 2000,
+   responsive: {
+      0: {
+         items: 1
+      },
+      600: {
+         items: 1
+      },
+      1000: {
+         items: 1
+      }
+   }
+})
